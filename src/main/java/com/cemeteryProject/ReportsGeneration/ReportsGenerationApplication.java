622fc9b2a41a9d51760828660bae04b8bcae2235
juanmz1606 <juan.1701920466@ucaldas.edu.co>
2025-04-21 14:56:30 -0500

    Commit inicial

---
+++ src/main/java/com/cemeteryProject/ReportsGeneration/ReportsGenerationApplication.java
@@ -0,0 +1,13 @@
+package com.cemeteryProject.ReportsGeneration;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ReportsGenerationApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ReportsGenerationApplication.class, args);
+	}
+
+}
